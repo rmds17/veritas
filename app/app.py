3012959a0e188d0ca97c096a68324fee054a9244
rmds <rmds@iscte-iul.pt>
2025-05-24 10:57:18 +0100

    setup de rotas e páginas no navbar

--- app/app.py
+++ app/app.py
@@ -8,10 +8,21 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/conta")
+def conta():
+    return render_template("conta.html")
 
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+@app.route("/factosg")
+def factosg():
+    return render_template("factosg.html")
+
+@app.route("/factosen")
+def factosen():
+    return render_template("factosen.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
